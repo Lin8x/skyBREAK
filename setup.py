37ecf96c08d06b350462e6619de01422e9572d6e
Lin8x <40849996+Lin8x@users.noreply.github.com>
2019-07-02 13:56:40 +0430

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ import time
 
 os.system("sudo mv skybreak /usr/share")
 os.system("sudo mv skybreak.desktop /usr/share/applications/")
-os.system("sudo mv skybreak.sh /usr/bin")
+os.system("sudo mv skybreak /usr/bin")
 time.sleep(3)
 print("Please type 'skybreak' in your terminal.")
